dfaa882322cf2c5cbf6a18e7ddc6c8bf86c9db25
keerthanakeeri56-create <keerthanakeeri56@gmail.com>
2026-05-06 13:16:30 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import qrcode
+from  PTL import Image
+
+st.title("Qr code generator")
+data=st.text_input("enter url")
+if st.button("Generate QR"):
+  if data:
+    qr = qrcode.make(data)
+    qr.save("qr.png")
+
+    img = image.open("qr.png")
+    st.image(img, caption="Generated QR Code")
+
+    with open("qr.png","rb") as f:
+        st.download_button("Downlosd QR",f,file_name="qr.png")
+  else:
+      st.warning("Please enter some text")
